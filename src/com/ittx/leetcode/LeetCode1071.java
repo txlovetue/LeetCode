7b6bc21c795690f295570a9ec805a3fdef63663f
ittx <goodStudy@ittx.com>
2020-03-12 22:08:03 +0800

    LeetCode1071题：字符串最大公约因子,添加注解

--- src/com/ittx/leetcode/LeetCode1071.java
+++ src/com/ittx/leetcode/LeetCode1071.java
@@ -14,6 +14,15 @@ import java.util.Scanner;
  * 输出："AB"
  */
 public class LeetCode1071 {
+    /**
+     * 检查截取字符拼接是否与传入值一致
+     * 例如：校验ASDC和ASDCASDC：
+     *      len = 2；"ASDC" + "ASDC" 与 "ASDCASDC"字面量的值一样
+     *    注意：比较字符串的时候需要使用equals方法，不能简单的使用"=="
+     * @param x
+     * @param s
+     * @return
+     */
     public static boolean check(String x, String s){
         int len =  s.length() / x.length();
         String base = "";
@@ -26,10 +35,12 @@ public class LeetCode1071 {
     public static String gcdOfStrings(String str1, String str2){
         int len1 = str1.length(), len2 = str2.length();
         for (int i = Math.min(len1, len2); i >= 1; --i){
+            // 取str1和str2长度的最小值，然后以最大值为基值（因为求最大公约因子）
+            //取到第一个两个str长度都可以除尽的数，就是最大公约数
             if (len1 % i == 0 && len2 % i == 0){
                 String substring = str1.substring(0, i);
-                System.out.println(check(substring, str1));
-                System.out.println(check(substring, str2));
+                //System.out.println(check(substring, str1));
+                //System.out.println(check(substring, str2));
                 if (check(substring, str1) && check(substring, str2)){
                     return substring;
                 }
